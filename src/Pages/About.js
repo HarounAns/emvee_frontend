69329cef6d531801992b5e02cafd5020fa31009d
harounans <hansari97@gmail.com>
2021-12-29 23:42:45 -0500

    update about section

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -5,8 +5,10 @@ const profilePic = 'https://www.smogon.com/forums/media/data/avatars/o/162/16270
 export default function Home() {
     return (
         <div>
-            <img src={profilePic} alt="smogonProfilePic"/>
-            <div style={{ margin: "20px 30vw", textAlign: 'left' }}>The Pokémon Master himself! Emvee started focusing his time on Pokémon content back in 2010, and since then has created a fun, thriving community around him. Found on team Tempo Storm, he’s a prominent member of the Smogon community, and has been seen at official Pokémon VGC regional events. He’s worked with various companies including Teespring, Champion, and is a #PokemonInfluencer for The Pokémon Company International. Outside of Pokémon, Emvee plays other games with his friends and spends time with his family.</div>
+            <img src={profilePic} alt="smogonProfilePic" />
+            <div style={{ margin: "20px 30vw", textAlign: 'left' }}>
+                As one of the greatest Pokémon team builders to touch the game, Emvee has proven that he can build a successful team around virtually any Pokémon. He started producing content towards the end of 2013 and since then has created a fun, thriving community founded on the belief that anyone can win with their favorite Pokémon. He has peaked the online ladder numerous times in his career and has a knack for thinking outside the box, making him a formidable opponent in creative formats such as Draft Leagues. Outside of Pokémon, Emvee enjoys going to nice restaurants, visiting wineries, hitting the gym, partying on the weekend, and hanging out with his friends and family.
+            </div>
         </div>
     )
 }
